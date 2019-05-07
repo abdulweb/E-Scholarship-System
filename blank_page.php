f147439128bb49521c2377befb84ab0212c77095
abumuhd <binraheem01@gmail.com>
2019-05-07 14:38:57 +0100

    starter page added

---
+++ blank_page.php
@@ -0,0 +1,38 @@
+
+<?php include('includes/headlink.php'); ?>
+	<!--
+		.boxed = boxed version
+	-->
+	<body>
+
+
+		<!-- WRAPPER -->
+		<div id="wrapper">
+
+		
+            <?php include('includes/navbar.php'); ?>
+
+			<?php include('includes/header.php');?>
+
+
+			<!-- MIDDLE -->
+			<section id="middle">
+
+				<div id="content" class="padding-20">
+
+					content here...
+
+				</div>
+			</section>
+			<!-- /MIDDLE -->
+
+		</div>
+
+
+
+	
+		<!-- JAVASCRIPT FILES -->
+		<?php include('includes/js.php'); ?>
+
+	</body>
+</html>
