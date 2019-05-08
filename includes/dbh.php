9cb0f59bae5dc0085e99be11087a41a50f7f227d
abumuhd <binraheem01@gmail.com>
2019-05-08 21:00:44 +0100

    password change in dbh class

--- includes/dbh.php
+++ includes/dbh.php
@@ -11,11 +11,10 @@ class dbh
 	public function connect(){
 		$this->servername ='localhost';
 		$this->username ='abdulweb';
-		$this->password ='rosot';
+		$this->password ='root';
 		$this->dbname ='zamScholarship';
 
 		$conn = new mysqli($this->servername,$this->username, $this->password,$this->dbname );
-
 		return $conn;
 	}
 
