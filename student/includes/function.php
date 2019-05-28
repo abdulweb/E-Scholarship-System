39fc7ea39477a036c5f07378908be547370e35e1
Olami001 <abdulrasheeda9@gmail.com>
2019-05-28 16:38:44 +0100

    function add to student end

---
+++ student/includes/function.php
@@ -0,0 +1,28 @@
+<?php
+	if (isset($_POST['submit button'])) {
+		// step one
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$middlename = $_POST['middlename'];
+		$dob = $_POST['dob'];
+		$phoneNo = $_POST['phoneNo'];
+		$gender = $_POST['gender'];
+		$maritalStatus = $_POST['maritalStatus'];
+		$religion = $_POST['religion'];
+
+		//step two Institution info
+		$admissionNo = $_POST['admissionNo'];
+		$institue = $_POST['institue'];
+		$faculty = $_POST['faculty'];
+		$department = $_POST['department'];
+		$level = $_POST['level'];
+
+		//step two Bank Info
+		$admissionNo = $_POST['admissionNo'];
+		$institue = $_POST['institue'];
+		$faculty = $_POST['faculty'];
+		$department = $_POST['department'];
+		$level = $_POST['level'];
+	}
+
+?>
